00d313624a083fa369d826221bfb0fade7e59f06
mrhacker679 <93563893+mrhacker679@users.noreply.github.com>
2023-04-01 20:38:57 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,16 +16,16 @@ with open(os.path.expanduser("~/.config/Code/User/settings.json"), "w") as f:
     f.write(json.dumps(settings))
 
 # Install extensions
-os.system('code --install-extension ms-python.python')
-os.system('code --install-extension formulahendry.code-runner')
-os.system('code --install-extension eamodio.gitlens')
+#os.system('code --install-extension ms-python.python')
+#os.system('code --install-extension formulahendry.code-runner')
+#os.system('code --install-extension eamodio.gitlens')
 
 
 
 ## Read extensions from file
-#with open('extensions.txt', 'r') as f:
-#    extensions = f.read().splitlines()
+with open('extensions.txt', 'r') as f:
+    extensions = f.read().splitlines()
 
 ## Install extensions
-#for extension in extensions:
-#    os.system(f'code --install-extension {extension}')
+for extension in extensions:
+    os.system(f'code --install-extension {extension}')
